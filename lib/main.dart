c599b20e7d6bd3eb8fb9e3bf40c7e3dcd1f19daf
Igor Guerra <igguerra07@gmail.com>
2023-05-01 15:37:00 -0400

    feat: get di env by app flavor

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:injectable/injectable.dart';
 
 import 'package:movie_app/src/app/di/di.dart';
 import 'package:movie_app/src/app/app_widget.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await configureDependencies();
+  final environment = await _getEnvironmentByFlavor();
+  await configureDependencies(environment);
   runApp(const MovieApp());
+}
+
+Future<String> _getEnvironmentByFlavor() async {
+  const platform = MethodChannel('flavor');
+  String? environment = await platform.invokeMethod<String>('getFlavor');
+  return environment ?? Environment.prod;
 }
